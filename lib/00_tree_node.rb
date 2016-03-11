f3a802c7265d9a4463f6cb89000982c8fffbf582
Ben Calabrese <bencalabrese@gmail.com>
2016-03-11 12:15:33 -0800

    added dfs and bfs (phase 4)

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -21,13 +21,38 @@ class PolyTreeNode
 
   def add_child(child_node)
     child_node.parent = self
-    @children << child_node unless @children.include?(child_node)
+    @children << child_node unless children.include?(child_node)
   end
 
   def remove_child(child)
-    raise "Not one of your children :(" unless @children.include?(child)
-    @children.delete(child)
+    raise "Not one of your children :(" unless children.include?(child)
+    children.delete(child)
     child.parent = nil
+  end
+
+  def dfs(target_value)
+    return self if value == target_value
+    return nil if children.empty?
+
+    children.each do |child|
+      child_hit = child.dfs(target_value)
+      return child_hit if child_hit
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    queue = [self]
+
+    until queue.empty?
+      el = queue.shift
+      queue += el.children
+
+      return el if el.value == target_value
+    end
+
+    nil
   end
 
 end
