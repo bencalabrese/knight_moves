56928978fe7ad20fd7d08bbcdfc23f6ce6c96619
Ben Calabrese <bencalabrese@gmail.com>
2016-03-11 11:57:00 -0800

    Fixed parent= and add_child and remove_child methds (Phase 2 & 3)

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -7,9 +7,27 @@ class PolyTreeNode
 
   attr_reader :value, :parent, :children
 
-  def parent=(parent_node_name)
-    @parent = parent_node_name
+  def parent=(new_parent_node)
+    parent.children.delete(self) unless parent.nil?
+
+    @parent = new_parent_node
 
     parent.children << self unless parent.nil?
   end
+
+  def inspect
+    @value.inspect
+  end
+
+  def add_child(child_node)
+    child_node.parent = self
+    @children << child_node unless @children.include?(child_node)
+  end
+
+  def remove_child(child)
+    raise "Not one of your children :(" unless @children.include?(child)
+    @children.delete(child)
+    child.parent = nil
+  end
+
 end
