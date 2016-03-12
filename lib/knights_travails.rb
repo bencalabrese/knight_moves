3bcd6b3e20f9cccd478c7a8c76aa074542739f0c
Ben Calabrese <bencalabrese@gmail.com>
2016-03-11 17:00:22 -0800

    Build move tree and find path

--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -1,3 +1,5 @@
+require_relative '00_tree_node'
+require 'byebug'
 class KnightPathFinder
   DELTAS = [
     [2, 1],
@@ -17,12 +19,14 @@ class KnightPathFinder
   end
 
   def initialize(starting_pos)
-    @starting_pos = starting_pos
     @visited_positions = [starting_pos]
+    @root_node = PolyTreeNode.new(starting_pos)
   end
 
+  attr_reader :root_node
+
   def new_move_positions(pos)
-    new_move_positions = self.valid_moves(pos)
+    new_move_positions = self.class.valid_moves(pos)
     new_move_positions.reject!{|pos| @visited_positions.include?(pos)}
 
     @visited_positions += new_move_positions
@@ -30,10 +34,30 @@ class KnightPathFinder
   end
 
   def build_move_tree
-    queue = new_move_positions(@starting_pos)
+    queue = [@root_node]
+
     until queue.empty?
-      pos = queue.shift
-      queue += new_move_positions(pos)
+      node = queue.shift
+
+      new_move_positions(node.value).each do |pos|
+        child_node = PolyTreeNode.new(pos)
+        queue << child_node
+        child_node.parent = node
+      end
     end
   end
+
+  def find_path(end_pos)
+    target_node = root_node.bfs(end_pos)
+    path = []
+    until target_node.parent.nil?
+      path.unshift(target_node.value)
+      target_node = target_node.parent
+    end
+    path.unshift(root_node.value)
+  end
+
 end
+knight = KnightPathFinder.new([0,0])
+knight.build_move_tree
+p knight.find_path([7,6])
